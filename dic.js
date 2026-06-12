4a5a170fedf4aa0e411033a21a426e5d66f34e60
Raúl Astorga <r.astorga12@gmail.com>
2026-06-12 16:42:27 -0700

    Se agregaron nuevas estadisticas

---
+++ dic.js
@@ -0,0 +1,14 @@
+const dic = {
+    "Mexico": "México", "South Africa": "Sudáfrica", "Korea Republic": "Corea del Sur", "Czechia": "República Checa",
+    "Canada": "Canadá", "Bosnia and Herzegovina": "Bosnia", "USA": "USA", "Paraguay": "Paraguay",
+    "Qatar": "Qatar", "Switzerland": "Suiza", "Brazil": "Brasil", "Morocco": "Marruecos",
+    "Haiti": "Haití", "Scotland": "Escocia", "Australia": "Australia", "Türkiye": "Turquía",
+    "Germany": "Alemania", "Curaçao": "Curazao", "Netherlands": "Países Bajos", "Japan": "Japón",
+    "Côte d'Ivoire": "Costa de Marfil", "Ecuador": "Ecuador", "Sweden": "Suecia", "Tunisia": "Túnez",
+    "Spain": "España", "Cabo Verde": "Cabo Verde", "Belgium": "Bélgica", "Egypt": "Egipto",
+    "Saudi Arabia": "Arabia S.", "Uruguay": "Uruguay", "IR Iran": "Irán", "New Zealand": "Nueva Zelanda",
+    "France": "Francia", "Senegal": "Senegal", "Iraq": "Irak", "Norway": "Noruega",
+    "Argentina": "Argentina", "Algeria": "Argelia", "Austria": "Austria", "Jordan": "Jordania",
+    "Portugal": "Portugal", "Congo DR": "RD Congo", "England": "Inglaterra", "Croatia": "Croacia",
+    "Ghana": "Ghana", "Panama": "Panamá", "Uzbekistan": "Uzbekistán", "Colombia": "Colombia"
+};
---
+++ equiposdatos.js
@@ -0,0 +1,18 @@
+const equiposDatos = {
+    "Uruguay": {d: "Jona", c: "uy", g: "H"}, "Túnez": {d: "Jona", c: "tn", g: "F"}, "Jordania": {d: "Jona", c: "jo", g: "J"},
+    "Australia": {d: "Jona", c: "au", g: "D"}, "Inglaterra": {d: "Jona", c: "gb-eng", g: "L"}, "Marruecos": {d: "Jona", c: "ma", g: "C"},
+    "México": {d: "Jona", c: "mx", g: "A"}, "Nueva Zelanda": {d: "Jona", c: "nz", g: "G"}, "Alemania": {d: "Jona", c: "de", g: "E"},
+    "RD Congo": {d: "Jona", c: "cd", g: "K"}, "Noruega": {d: "Jona", c: "no", g: "I"}, "Canadá": {d: "Jona", c: "ca", g: "B"},
+    "Cabo Verde": {d: "David", c: "cv", g: "H"}, "Japón": {d: "David", c: "jp", g: "F"}, "Austria": {d: "David", c: "at", g: "J"},
+    "USA": {d: "David", c: "us", g: "D"}, "Panamá": {d: "David", c: "pa", g: "L"}, "Haití": {d: "David", c: "ht", g: "C"},
+    "República Checa": {d: "David", c: "cz", g: "A"}, "Irán": {d: "David", c: "ir", g: "G"}, "Costa de Marfil": {d: "David", c: "ci", g: "E"},
+    "Portugal": {d: "David", c: "pt", g: "K"}, "Francia": {d: "David", c: "fr", g: "I"}, "Bosnia": {d: "David", c: "ba", g: "B"},
+    "España": {d: "Raúl", c: "es", g: "H"}, "Suecia": {d: "Raúl", c: "se", g: "F"}, "Argelia": {d: "Raúl", c: "dz", g: "J"},
+    "Turquía": {d: "Raúl", c: "tr", g: "D"}, "Ghana": {d: "Raúl", c: "gh", g: "L"}, "Brasil": {d: "Raúl", c: "br", g: "C"},
+    "Sudáfrica": {d: "Raúl", c: "za", g: "A"}, "Egipto": {d: "Raúl", c: "eg", g: "G"}, "Curazao": {d: "Raúl", c: "cw", g: "E"},
+    "Colombia": {d: "Raúl", c: "co", g: "K"}, "Senegal": {d: "Raúl", c: "sn", g: "I"}, "Suiza": {d: "Raúl", c: "ch", g: "B"},
+    "Arabia S.": {d: "Sergio", c: "sa", g: "H"}, "Países Bajos": {d: "Sergio", c: "nl", g: "F"}, "Argentina": {d: "Sergio", c: "ar", g: "J"},
+    "Paraguay": {d: "Sergio", c: "py", g: "D"}, "Croacia": {d: "Sergio", c: "hr", g: "L"}, "Escocia": {d: "Sergio", c: "gb-sct", g: "C"},
+    "Corea del Sur": {d: "Sergio", c: "kr", g: "A"}, "Bélgica": {d: "Sergio", c: "be", g: "G"}, "Ecuador": {d: "Sergio", c: "ec", g: "E"},
+    "Uzbekistán": {d: "Sergio", c: "uz", g: "K"}, "Irak": {d: "Sergio", c: "iq", g: "I"}, "Qatar": {d: "Sergio", c: "qa", g: "B"}
+};
