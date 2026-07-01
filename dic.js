d76eb2bba011d2d3fd7f100f5ca58e9033516815
Raúl Astorga <r.astorga12@gmail.com>
2026-06-30 17:11:06 -0700

    Se agrega mejora para brackets ordenados

--- dic.js
+++ dic.js
@@ -1,11 +1,11 @@
 const dic = {
     "Mexico": "México", "South Africa": "Sudáfrica", "Korea Republic": "Corea", "Czechia": "Rep. Checa",
-    "Canada": "Canadá", "Bosnia and Herzegovina": "Bosnia", "USA": "USA", "Paraguay": "Paraguay",
+    "Canada": "Canadá", "Bosnia and Herzegovina": "Bosnia", "Bosnia y Herzegovina": "Bosnia", "USA": "USA", "EE. UU.": "USA", "Paraguay": "Paraguay",
     "Qatar": "Qatar", "Switzerland": "Suiza", "Brazil": "Brasil", "Morocco": "Marruecos",
     "Haiti": "Haití", "Scotland": "Escocia", "Australia": "Australia", "Türkiye": "Turquía",
     "Germany": "Alemania", "Curaçao": "Curazao", "Netherlands": "Países Bajos", "Japan": "Japón",
     "Côte d'Ivoire": "Costa de Marfil", "Ecuador": "Ecuador", "Sweden": "Suecia", "Tunisia": "Túnez",
-    "Spain": "España", "Cabo Verde": "Cabo Verde", "Belgium": "Bélgica", "Egypt": "Egipto",
+    "Spain": "España", "Cabo Verde": "Cabo Verde", "Islas de Cabo Verde": "Cabo Verde", "Belgium": "Bélgica", "Egypt": "Egipto",
     "Saudi Arabia": "Arabia S.", "Uruguay": "Uruguay", "IR Iran": "Irán", "New Zealand": "N. Zelanda",
     "France": "Francia", "Senegal": "Senegal", "Iraq": "Irak", "Norway": "Noruega",
     "Argentina": "Argentina", "Algeria": "Argelia", "Austria": "Austria", "Jordan": "Jordania",
